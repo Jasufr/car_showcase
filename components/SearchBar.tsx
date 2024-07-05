36358ec3d9eab87a06a5a39705f700eb76d886fa
justin etienne <juetienne7@gmail.com>
2024-07-05 17:43:24 +0900

    search part started

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,14 @@
 'use client';
 
+import Image from "next/image";
 import SearchManufacturer from "./SearchManufacturer";
 import { useState } from "react";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string}) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image src="/magnifying-glass.svg" alt="magnifying glass" width={40} height={40} className="object-contain" />
+  </button>
+);
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('');
@@ -14,6 +21,7 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
     </form>
   )
