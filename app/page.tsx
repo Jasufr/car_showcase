22de2a243584350aa2f956b447690a2bf0c7bcfe
justin etienne <juetienne7@gmail.com>
2024-07-09 15:41:53 +0900

    show more button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fuels, manufacturers, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
@@ -35,6 +35,10 @@ export default async function Home({ searchParams }) {
             <div className="home__cars-wrapper">
               {allCars?.map((car) => (<CarCard car={car} />))}
             </div>
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className="home__error-container">
